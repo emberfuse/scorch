30984404c2e97da1f6cc89bf9bbd38c09b9b7cf6
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-01-23 21:23:30 +0530

    Add login view route

--- routes/routes.php
+++ routes/routes.php
@@ -1,12 +1,17 @@
 <?php
 
-use Citadel\Auth\Config;
+use Citadel\Citadel\Config;
 use Illuminate\Support\Facades\Route;
 use Citadel\Http\Controllers\AuthenticationController;
+use Citadel\Http\Controllers\TwoFactorAuthenticationController;
 
 Route::group([
-    'middleware' => Config::middleware(['web'])
-], function () {
+    'middleware' => Config::middleware(['web']),
+], function (): void {
+    Route::get('/login', [AuthenticationController::class, 'create'])->name('login');
     Route::post('/login', [AuthenticationController::class, 'store']);
     Route::post('/logout', [AuthenticationController::class, 'destroy']);
+
+    // Route::get('/two-factor-challenge', [TwoFactorAuthenticationController::class, 'create'])->name('two-factor.login');
+    // Route::post('/two-factor-challenge', [TwoFactorAuthenticationController::class, 'store']);
 });
