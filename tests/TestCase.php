836f8c1ff979feb1e8f9818ac6e7e760355b962f
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-01-23 21:21:42 +0530

    Add environment setup to base test case class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,19 +2,35 @@
 
 namespace Citadel\Tests;
 
-use Mockery;
+use Mockery as m;
 use Citadel\Providers\CitadelServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use Citadel\Tests\Fixtures\TestAuthenticationUser;
 
 abstract class TestCase extends BaseTestCase
 {
     public function tearDown(): void
     {
-        Mockery::close();
+        m::close();
     }
 
     protected function getPackageProviders($app)
     {
         return [CitadelServiceProvider::class];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['migrator']->path(__DIR__ . '/../database/migrations');
+
+        $app['config']->set('auth.providers.users.model', TestAuthenticationUser::class);
+
+        $app['config']->set('database.default', 'testbench');
+
+        $app['config']->set('database.connections.testbench', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
     }
 }
