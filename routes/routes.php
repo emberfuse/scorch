2405cadceca0a894f4c10c93a704b6839bbbe8e7
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-01-23 21:56:10 +0530

    Categorize routes

--- routes/routes.php
+++ routes/routes.php
@@ -8,10 +8,16 @@ use Citadel\Http\Controllers\TwoFactorAuthenticationController;
 Route::group([
     'middleware' => Config::middleware(['web']),
 ], function (): void {
-    Route::get('/login', [AuthenticationController::class, 'create'])->name('login');
-    Route::post('/login', [AuthenticationController::class, 'store']);
-    Route::post('/logout', [AuthenticationController::class, 'destroy']);
+    Route::group(['middleware' => ['guest']], function (): void {
+        Route::get('/login', [AuthenticationController::class, 'create'])->name('login');
+        Route::post('/login', [AuthenticationController::class, 'store']);
 
-    // Route::get('/two-factor-challenge', [TwoFactorAuthenticationController::class, 'create'])->name('two-factor.login');
-    // Route::post('/two-factor-challenge', [TwoFactorAuthenticationController::class, 'store']);
+        Route::get('/two-factor-challenge', [TwoFactorAuthenticationController::class, 'create'])->name('two-factor.login');
+    });
+
+    Route::group(['middleware' => ['auth']], function (): void {
+        Route::post('/logout', [AuthenticationController::class, 'destroy']);
+
+        Route::post('/two-factor-challenge', [TwoFactorAuthenticationController::class, 'store']);
+    });
 });
