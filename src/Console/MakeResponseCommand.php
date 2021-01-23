49e7d8ba31361dd36a99f5703164c673908dc5fd
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-01-23 22:03:43 +0530

    Add make response generator command

--- src/Console/MakeResponseCommand.php
+++ src/Console/MakeResponseCommand.php
@@ -2,8 +2,73 @@
 
 namespace Citadel\Console;
 
-use Illuminate\Console\Command;
+use Illuminate\Console\GeneratorCommand;
 
-class MakeResponseCommand extends Command
+class MakeResponseCommand extends GeneratorCommand
 {
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:response';
+
+    /**
+     * The name of the console command.
+     *
+     * This name is used to identify the command during lazy loading.
+     *
+     * @var string|null
+     */
+    protected static $defaultName = 'make:response';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new response class';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Response';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return $this->resolveStubPath('/stubs/response.stub');
+    }
+
+    /**
+     * Resolve the fully-qualified path to the stub.
+     *
+     * @param string $stub
+     *
+     * @return string
+     */
+    protected function resolveStubPath(string $stub): string
+    {
+        return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
+            ? $customPath
+            : __DIR__ . $stub;
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param string $rootNamespace
+     *
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . '\Http\Responses';
+    }
 }
