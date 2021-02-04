87268cfde4fb7b33fccf36d3ee0dff82e1a88edf
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-04 23:28:28 +0530

    Add optional content property to response factory

--- src/Http/Responses/Response.php
+++ src/Http/Responses/Response.php
@@ -6,9 +6,36 @@ use Illuminate\Routing\Redirector;
 use Illuminate\Http\RedirectResponse;
 use Cratespace\Citadel\Citadel\Config;
 use Illuminate\Routing\ResponseFactory;
+use Illuminate\View\Factory as ViewFactory;
 
 abstract class Response extends ResponseFactory
 {
+    /**
+     * Optional content.
+     *
+     * @var mixed|null
+     */
+    protected $content;
+
+    /**
+     * Create a new response factory instance.
+     *
+     * @param \Illuminate\Contracts\View\Factory $view
+     * @param \Illuminate\Routing\Redirector     $redirector
+     * @param mixed|null                         $content
+     *
+     * @return void
+     */
+    public function __construct(
+        ViewFactory $view,
+        Redirector $redirector,
+        $content = null
+    ) {
+        $this->view = $view;
+        $this->redirector = $redirector;
+        $this->content = $content;
+    }
+
     /**
      * Full URL path to home route.
      *
@@ -32,9 +59,10 @@ abstract class Response extends ResponseFactory
     /**
      * Create a new redirect response to the previous location.
      *
-     * @param  int  $status
-     * @param  array  $headers
-     * @param  mixed  $fallback
+     * @param int   $status
+     * @param array $headers
+     * @param mixed $fallback
+     *
      * @return \Illuminate\Http\RedirectResponse
      */
     public function back($status = 302, $headers = [], $fallback = false): RedirectResponse
