1aff6212c61fc99b09e1806d94ff6f2cc11d0e9b
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-08 07:19:06 +0530

    Add register actions trait for service providers

---
+++ src/Providers/Traits/HasActions.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Cratespace\Sentinel\Providers\Traits;
+
+trait HasActions
+{
+    /**
+     * Register all action classes in the given array.
+     *
+     * @return void
+     */
+    public function registerActions(): void
+    {
+        if (! property_exists($this, 'actions')) {
+            return;
+        }
+
+        collect($this->actions)->each(
+            fn ($concrete, $abstract) => $this->app->singleton($abstract, $concrete)
+        );
+    }
+}
