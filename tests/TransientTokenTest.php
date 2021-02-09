dcbe5c4a8c8a42ac978cb7c5a4153490719f503b
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-09 21:06:03 +0530

    Add complete test suite for sentinel SPA authentication feature

---
+++ tests/TransientTokenTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Cratespace\Sentinel\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Cratespace\Sentinel\Auth\Tokens\TransientToken;
+
+class TransientTokenTest extends TestCase
+{
+    public function testCanDetermineWhatItCanAndCantDo()
+    {
+        $token = new TransientToken();
+
+        $this->assertTrue($token->can('foo'));
+        $this->assertTrue($token->can('bar'));
+        $this->assertFalse($token->cant('foo'));
+        $this->assertFalse($token->cant('bar'));
+    }
+}
