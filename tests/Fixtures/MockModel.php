5cc3be7a8f730a461194388ea91f967d8c62ceb6
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-19 23:40:27 +0530

    Add mock model class

---
+++ tests/Fixtures/MockModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Cratespace\Sentinel\Tests\Fixtures;
+
+use Illuminate\Database\Eloquent\Model;
+use Cratespace\Sentinel\Models\Traits\Redirectable;
+
+class MockModel extends Model
+{
+    use Redirectable;
+
+    protected $table = 'mock';
+
+    /**
+     * Route name of single resource.
+     *
+     * @var string
+     */
+    protected $resourceView = 'show';
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'id';
+    }
+}
