5fe57716303a5819d16b9ddb6da2897094903337
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-22 09:38:57 +0530

    Add scenario to Password rule and adjust logic

--- tests/PasswordRuleTest.php
+++ tests/PasswordRuleTest.php
@@ -51,4 +51,18 @@ class PasswordRuleTest extends TestCase
         $this->assertTrue(Str::contains($rule->message(), 'must be at least 8 characters'));
         $this->assertTrue(Str::contains($rule->message(), 'special character'));
     }
+
+    public function testPasswordRuleCanRequireNumericAndSpecialCharacters()
+    {
+        $rule = new PasswordRule();
+
+        $rule->length(10)->requireNumeric()->requireSpecialCharacter();
+
+        $this->assertTrue($rule->passes('password', 'password5%'));
+        $this->assertFalse($rule->passes('password', 'my-password'));
+
+        $this->assertTrue(Str::contains($rule->message(), 'must be at least 10 characters'));
+        $this->assertTrue(Str::contains($rule->message(), 'contain at least one special character'));
+        $this->assertTrue(Str::contains($rule->message(), 'and one number'));
+    }
 }
